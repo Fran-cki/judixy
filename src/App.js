a6f18f6df7ca94c6ec30f53cc6d5777241b0f54f
Fran-cki <vsfrancki@gmail.com>
2025-05-18 16:47:29 +0300

    Maj 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Mise A jour
+          Maj 2
         </p>
         <a
           className="App-link"
